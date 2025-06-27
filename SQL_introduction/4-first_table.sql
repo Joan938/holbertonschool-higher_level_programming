1dc4019afad0df3bd84f1f37e2aa1f474533e836
Joan938 <martinezibarrondojoan@gmail.com>
2025-06-27 15:14:25 -0400

    Making tasks 3-5

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- 4. First table
+-- Creates a table named first_table with columns id and name if it doesn’t already exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- 5. Full description
+-- Prints the CREATE TABLE statement for first_table
+SHOW CREATE TABLE first_table;
