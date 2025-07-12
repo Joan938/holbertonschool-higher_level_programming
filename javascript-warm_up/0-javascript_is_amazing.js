3e3cd60ad04be27401dc7671ebd758663152326f
Joan938 <martinezibarrondojoan@gmail.com>
2025-07-12 10:43:28 -0400

    Making some changes

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,2 +1,5 @@
-enable myVar = "Javasript is amazing"
-console.log(myVar)
+#!/usr/bin/node
+
+// Create a constant variable with the required message
+enable myVar = "JavaScript is amazing" // semistandard compatible
+console.log(myVar);
